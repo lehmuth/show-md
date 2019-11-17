be158599c681c6b2c04ad509ec3840011a5336d5
lehmuth <56397534+lehmuth@users.noreply.github.com>
2019-11-17 22:19:48 +0100

    Update logging

--- bin/show-md.js
+++ bin/show-md.js
@@ -1,27 +1,37 @@
 #!/usr/bin/env node
-const ShowmdServer = require('../index.js');
-const showmd = new ShowmdServer();
-//showmd.  log.setLevel("all");
-showmd.config.on('warning', function(msg){
-  showmd.log.warn(msg);
-});
+//loading and parsing arguments
+const parseArgs = require('minimist');
+const argv = parseArgs(process.argv.slice(2));
 
+//init logging
+const path = require('path');
+var logPath = path.join(__dirname, "../logs/system.log");
+const log = require('simple-node-logger').createSimpleLogger(logPath);
+//log.setLevel("all");
+
+//init Server
+const ShowMdServer = require('../index.js');
+const showmd = new ShowMdServer();
+showmd.on('warning', (msg) => {log.warn(msg);});
+showmd.on('error', (msg) => {log.error(msg);});
+showmd.on('started', () => {log.info("Server started on http://localhost:" + showmd.config.getPort());});
+showmd.on('stoped', () => {log.info("Server stoped.");});
+
+
+log.debug("Starting server...");
 showmd.start();
-
-showmd.log.info("Server starting...");
-showmd.log.info("loading configuration...");
+log.debug("loading configuration...");
 const attrHandler = require('../src/attr_handler.js');
 var args = process.argv.slice(2);
 attrHandler.parseArguments(args, showmd);
-showmd.log.debug("Configuration loaded:");
-showmd.log.debug("Current root directory: " + showmd.config.getRootPath());
-showmd.log.debug("htdocs directory: " + showmd.config.getHtdocs());
-showmd.log.debug("Current language: " + showmd.config.getLanguage());
-showmd.log.debug("Current stylesheet: " + showmd.config.getStylesheet());
-showmd.log.debug("Current Port: " + showmd.config.getPort());
-showmd.log.debug("Current http log path: " + showmd.config.getHttpLogPath());
-showmd.log.debug("Current system log path: " + showmd.config.getLogPath());
-showmd.log.info("Server ready on http://localhost:" + showmd.config.getPort());
+log.debug("Configuration loaded:");
+log.debug("Current root directory: " + showmd.config.getRootPath());
+log.debug("htdocs directory: " + showmd.config.getHtdocs());
+log.debug("Current language: " + showmd.config.getLanguage());
+log.debug("Current stylesheet: " + showmd.config.getStylesheet());
+log.debug("Current Port: " + showmd.config.getPort());
+log.debug("Current http log path: " + showmd.config.getHttpLogPath());
+log.debug("Current system log path: " + logPath);
 
 
 const cmdHandler = require('../src/cmd_handler.js')(showmd);
