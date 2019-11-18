86670509b63f61f54196ccf1bf389ef6ba2d818b
lehmuth <56397534+lehmuth@users.noreply.github.com>
2019-11-18 02:49:01 +0100

    Optimized show-md.js flow

--- bin/show-md.js
+++ bin/show-md.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 //loading and parsing arguments
+const showmd = require('../index.js');
 const parseArgs = require('minimist');
 const argv = parseArgs(process.argv.slice(2));
-
 console.log(argv);
+if(argv._[0] !== undefined){
+
+}
+//old argument parser
+const attrHandler = require('../src/attr_handler.js');
+var args = process.argv.slice(2);
+attrHandler.parseArguments(args, showmd);
 
 //init logging
 const path = require('path');
@@ -11,21 +18,6 @@ var logPath = path.join(__dirname, "../logs/system.log");
 const log = require('simple-node-logger').createSimpleLogger(logPath);
 //log.setLevel("all");
 
-//init Server
-const ShowMdServer = require('../index.js');
-const showmd = new ShowMdServer();
-showmd.on('warning', (msg) => {log.warn(msg);});
-showmd.on('error', (msg) => {log.error(msg);});
-showmd.on('started', () => {log.info("Server started on http://localhost:" + showmd.config.getPort());});
-showmd.on('stoped', () => {log.info("Server stoped.");});
-
-
-log.debug("Starting server...");
-showmd.start();
-log.debug("loading configuration...");
-const attrHandler = require('../src/attr_handler.js');
-var args = process.argv.slice(2);
-attrHandler.parseArguments(args, showmd);
 log.debug("Configuration loaded:");
 log.debug("Current root directory: " + showmd.config.getRootPath());
 log.debug("htdocs directory: " + showmd.config.getHtdocs());
@@ -35,6 +27,16 @@ log.debug("Current Port: " + showmd.config.getPort());
 log.debug("Current http log path: " + showmd.config.getHttpLogPath());
 log.debug("Current system log path: " + logPath);
 
-
+//init command listener
 const cmdHandler = require('../src/cmd_handler.js')(showmd);
 cmdHandler.start();
+
+//init Server
+showmd.on('warning', (msg) => {log.warn(msg);});
+showmd.on('error', (msg) => {log.error(msg);});
+showmd.on('started', () => {log.info("Server started on http://localhost:" + showmd.config.getPort());});
+showmd.on('stoped', () => {log.info("Server stoped.");});
+
+//start Server
+log.debug("Starting server...");
+showmd.start();
