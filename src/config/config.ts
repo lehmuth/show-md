212207683f4c5b5fbb4bca9ca55a9b082ca840a6
Dominik Lehmuth <dominiklehmuth@gmail.com>
2020-07-15 03:25:32 +0200

    Hotfix: Fixed import problem with default.json

--- src/config/config.ts
+++ src/config/config.ts
@@ -19,52 +19,54 @@ export class ShowMdConfig extends EventEmitter {
   PUBLIC: string = 'PUBLIC';
   LIBS: string = 'LIBS';
 
-  constructor(){
+  constructor() {
     super();
-    let defaultConfig = require('./default.json');
+    let defaultConfig = require(__dirname + '/default.json');
     this.language = defaultConfig.language ?? 'en';
     this.stylesheetName = defaultConfig.stylesheetName ?? 'github';
     this.port = defaultConfig.port ?? 56657;
-    this.includeExtensions = defaultConfig.includeExtensions ?? ['.html', '.md', '.txt'];
-    this.stylesheets = defaultConfig.stylesheets as [string, string][] ?? [
-      ["default", "{APP_ROOT}/build/public/default.css"],
-      ["github", "{APP_ROOT}/libs/github.css"],
-      ["none", "{APP_ROOT}/build/public/none.css"]
+    this.includeExtensions = defaultConfig.includeExtensions ?? [
+      '.html',
+      '.md',
+      '.txt',
     ];
-    this.pathVariables = defaultConfig.pathVariables as [string, string][] ?? [
-      ["LOGS", path.resolve(__dirname, '../../logs')]
+    this.stylesheets = (defaultConfig.stylesheets as [string, string][]) ?? [
+      ['default', '{APP_ROOT}/build/public/default.css'],
+      ['github', '{APP_ROOT}/libs/github.css'],
+      ['none', '{APP_ROOT}/build/public/none.css'],
     ];
+    this.pathVariables = (defaultConfig.pathVariables as [
+      string,
+      string
+    ][]) ?? [['LOGS', path.resolve(__dirname, '../../logs')]];
     this.pathVariables.push(
       [this.APP_ROOT, path.resolve(__dirname, '../../')],
-      [this.HTDOCS, path.resolve(__dirname, "../../htdocs")],
+      [this.HTDOCS, path.resolve(__dirname, '../../htdocs')],
       [this.SERVER_ROOT, path.resolve(process.cwd())],
-      [this.PUBLIC, path.resolve(__dirname, "../../build/public")],
-      [this.LIBS, path.resolve(__dirname, "../../libs")]
+      [this.PUBLIC, path.resolve(__dirname, '../../build/public')],
+      [this.LIBS, path.resolve(__dirname, '../../libs')]
     );
-    this.htDirs = defaultConfig.htDirs ?? [
-      "{SERVER_ROOT}",
-      "{HTDOCS}"
+    this.htDirs = defaultConfig.htDirs ?? ['{SERVER_ROOT}', '{HTDOCS}'];
+    this.defaultFile = defaultConfig.defaultFile ?? 'README.md';
+    this.errorFiles = (defaultConfig.errorFiles as [number, string][]) ?? [
+      [403, '{HTDOCS}/error/error403.md'],
+      [404, '{HTDOCS}/error/error404.md'],
+      [500, '{HTDOCS}/error/error500.md'],
     ];
-    this.defaultFile = defaultConfig.defaultFile ?? "README.md";
-    this.errorFiles = defaultConfig.errorFiles as [number, string][] ?? [
-      [403, "{HTDOCS}/error/error403.md"],
-      [404, "{HTDOCS}/error/error404.md"],
-      [500, "{HTDOCS}/error/error500.md"]
-    ]
   }
 
-  getPath (id: string): string{
-    for(let pathTouple of this.pathVariables) {
-      if(pathTouple[0] === id){
+  getPath(id: string): string {
+    for (let pathTouple of this.pathVariables) {
+      if (pathTouple[0] === id) {
         return pathTouple[1];
       }
     }
     throw new Error('Undefined path to variable: ' + id);
   }
 
-  setPath (id: string, value: string): ShowMdConfig {
-    for(let pathTouple of this.pathVariables) {
-      if(pathTouple[0] === id){
+  setPath(id: string, value: string): ShowMdConfig {
+    for (let pathTouple of this.pathVariables) {
+      if (pathTouple[0] === id) {
         pathTouple[1] = path.resolve(value);
         return this;
       }
@@ -74,61 +76,61 @@ export class ShowMdConfig extends EventEmitter {
   }
 
   replacePathVariables(target: string): string {
-    return path.resolve( 
-      target.replace(/^{(.*)}/,
-        (a:string, variable: string): string => {
-          return this.getPath(variable); 
-        }
-      )
+    return path.resolve(
+      target.replace(/^{(.*)}/, (a: string, variable: string): string => {
+        return this.getPath(variable);
+      })
     );
   }
 
-  setLanguage (language: string): ShowMdConfig {
+  setLanguage(language: string): ShowMdConfig {
     this.language = language;
     return this;
   }
 
-  getLanguage (): string {
+  getLanguage(): string {
     return this.language;
   }
 
-  setStylesheet (stylesheet: string): ShowMdConfig {
+  setStylesheet(stylesheet: string): ShowMdConfig {
     this.stylesheetName = stylesheet;
     return this;
   }
 
-  getStylesheet (): string {
+  getStylesheet(): string {
     return this.stylesheetName;
   }
 
-  getDefinedStylesheets (): string[] {
-    return this.stylesheets.map((elem) => {return elem[0];});
+  getDefinedStylesheets(): string[] {
+    return this.stylesheets.map((elem) => {
+      return elem[0];
+    });
   }
 
-  getStylesheetPath (name: string): string {
-    for(let styleTouple of this.stylesheets){
-      if(styleTouple[0] === name){
+  getStylesheetPath(name: string): string {
+    for (let styleTouple of this.stylesheets) {
+      if (styleTouple[0] === name) {
         return this.replacePathVariables(styleTouple[1]);
       }
     }
     throw new Error('Undefined stylesheet: ' + name);
   }
 
-  setPort (port: number): ShowMdConfig {
+  setPort(port: number): ShowMdConfig {
     this.port = port;
     return this;
   }
 
-  getPort (): number {
+  getPort(): number {
     return this.port;
   }
 
-  setIncludeExtensions (includeExtensions: string[]): ShowMdConfig {
+  setIncludeExtensions(includeExtensions: string[]): ShowMdConfig {
     this.includeExtensions = includeExtensions;
     return this;
   }
 
-  addIncludeExtension (extension: string): ShowMdConfig {
+  addIncludeExtension(extension: string): ShowMdConfig {
     this.includeExtensions.push(extension);
     return this;
   }
@@ -139,38 +141,38 @@ export class ShowMdConfig extends EventEmitter {
     return this;
   }
 
-  getIncludeExtensions (): string[] {
+  getIncludeExtensions(): string[] {
     return this.includeExtensions;
   }
 
-  getHtDirs (): string[] {
+  getHtDirs(): string[] {
     return this.htDirs.map((dir): string => {
       return this.replacePathVariables(dir);
     });
-  } 
+  }
 
-  setHtDirs (htDirs: string[]): ShowMdConfig {
+  setHtDirs(htDirs: string[]): ShowMdConfig {
     this.htDirs = htDirs;
     return this;
   }
 
-  addHtDir (htDir: string): ShowMdConfig {
+  addHtDir(htDir: string): ShowMdConfig {
     this.htDirs.push(htDir);
     return this;
   }
 
-  getDefaultFile (): string {
+  getDefaultFile(): string {
     return this.defaultFile;
   }
 
-  setDefaultFile (defaultFile: string): ShowMdConfig {
+  setDefaultFile(defaultFile: string): ShowMdConfig {
     this.defaultFile = defaultFile;
     return this;
   }
 
-  setErrorFile (errorCode: number, file: string): ShowMdConfig {
-    for(let errTouple of this.errorFiles) {
-      if(errTouple[0] === errorCode){
+  setErrorFile(errorCode: number, file: string): ShowMdConfig {
+    for (let errTouple of this.errorFiles) {
+      if (errTouple[0] === errorCode) {
         errTouple[1] = path.resolve(file);
         return this;
       }
@@ -179,12 +181,12 @@ export class ShowMdConfig extends EventEmitter {
     return this;
   }
 
-  getErrorFile (errorCode: number): string {
-    for(let errTouple of this.errorFiles) {
-      if(errTouple[0] === errorCode){
+  getErrorFile(errorCode: number): string {
+    for (let errTouple of this.errorFiles) {
+      if (errTouple[0] === errorCode) {
         return this.replacePathVariables(errTouple[1]);
       }
     }
     throw new Error('Undefined path for error ' + errorCode);
   }
-};
+}
