9c4b621acfdd7164b0a8905703200f1a60b976e0
Dominik Lehmuth <dominiklehmuth@gmail.com>
2020-07-15 04:19:38 +0200

    Changed path to resources dir

--- src/md-parser/md_parser.ts
+++ src/md-parser/md_parser.ts
@@ -10,7 +10,7 @@ import { fileURLToPath } from 'url';
 export class ShowMdParser extends EventEmitter {
   config: ShowMdConfig;
   converter: Converter;
-  constructor (config?: ShowMdConfig) { 
+  constructor(config?: ShowMdConfig) {
     super();
     this.config = config ?? new ShowMdConfig();
     // Initialize and configure showdown converter
@@ -21,40 +21,43 @@ export class ShowMdParser extends EventEmitter {
   }
 
   private initConverter(build: boolean): Converter {
-    let extensions = []
+    let extensions = [];
     extensions.push('include');
-    if(build) {
+    if (build) {
       extensions.push('replaceLinks');
     }
 
     let converter = new Converter({ extensions });
-    converter.setOption('customizedHeaderId', true);				// makes it possible to select a custom header id with {custom_id}
-    converter.setOption('tables', true);							// enable tables
-    converter.setOption('ghCompatibleHeaderId', true);				// generates header ids compatible with github style (spaces are replaced with dashes and a bunch of non alphanumeric chars are removed)
-    converter.setOption('parseImgDimensions', true);				// makes it possible to add dimensions to image includes
-    converter.setOption('tasklists', true);						// enables the option of tasklists with [ ] and [x]
-    converter.setOption('emoji', true);							// enables emoji import
-    converter.setOption('underline', true);						// enables underline with __
-    converter.setOption('strikethrough', true);					// enables strikethrew with ~~
-    converter.setOption('includeExtensions', this.config.getIncludeExtensions());
+    converter.setOption('customizedHeaderId', true); // makes it possible to select a custom header id with {custom_id}
+    converter.setOption('tables', true); // enable tables
+    converter.setOption('ghCompatibleHeaderId', true); // generates header ids compatible with github style (spaces are replaced with dashes and a bunch of non alphanumeric chars are removed)
+    converter.setOption('parseImgDimensions', true); // makes it possible to add dimensions to image includes
+    converter.setOption('tasklists', true); // enables the option of tasklists with [ ] and [x]
+    converter.setOption('emoji', true); // enables emoji import
+    converter.setOption('underline', true); // enables underline with __
+    converter.setOption('strikethrough', true); // enables strikethrew with ~~
+    converter.setOption(
+      'includeExtensions',
+      this.config.getIncludeExtensions()
+    );
 
     return converter;
   }
 
-  setFilePath (filename: string): void {
+  setFilePath(filename: string): void {
     filename = path.dirname(path.resolve(filename));
     this.converter.setOption('fileDir', filename);
   }
 
   // Parse a markdown content into html content
-  mdToHtml (md: string): string {
+  mdToHtml(md: string): string {
     // Use showdown parser
     let htmlContent = this.converter.makeHtml(md);
     return this.generateHtmlFile(htmlContent);
   }
 
   // Generate html file
-  generateHtmlFile (content: string): string {
+  generateHtmlFile(content: string): string {
     let html = `
 <!DOCTYPE html>
 <html lang="${this.config.getLanguage()}">
@@ -72,60 +75,59 @@ export class ShowMdParser extends EventEmitter {
     return html;
   }
 
-  build (inputDir: string, outputDir: string): boolean {
+  build(inputDir: string, outputDir: string): boolean {
     let inputStats = fs.statSync(inputDir);
-    
-    if(!inputStats.isDirectory()) {
+
+    if (!inputStats.isDirectory()) {
       console.log('Your specified input is no directory!');
     }
-    
-    if(fs.existsSync(outputDir)) {
+
+    if (fs.existsSync(outputDir)) {
       let outputStats = fs.statSync(outputDir);
-      if(!outputStats.isDirectory()) {
+      if (!outputStats.isDirectory()) {
         console.log('The specified output path is no directory!');
         return false;
       }
-
     } else {
-      fs.mkdirSync(outputDir)
+      fs.mkdirSync(outputDir);
     }
 
     fs.readdir(inputDir, (err, files) => {
-        //handling error
-        if (err) {
-            console.log('Unable to scan input directory: ' + inputDir);
-            return false;
+      //handling error
+      if (err) {
+        console.log('Unable to scan input directory: ' + inputDir);
+        return false;
+      }
+
+      this.converter = this.initConverter(true);
+      //listing all files
+      files.forEach((file) => {
+        let fileInputPath = path.resolve(inputDir, file);
+        let fileOutputPath = path.resolve(outputDir, file);
+        let stat = fs.statSync(fileInputPath);
+        if (stat.isDirectory()) {
+          this.build(fileInputPath, fileOutputPath);
+        } else if (file.match(/.*\.(md)|(MD)$/i)) {
+          let data = fs.readFileSync(fileInputPath, 'utf-8');
+          this.setFilePath(inputDir);
+          // Rename Readme.md to index.md
+          fileOutputPath.replace(/.*(README).md$/i, '$1index$2');
+          // Rename markdown file to html file
+          fileOutputPath = fileOutputPath.slice(0, -2) + 'html';
+          fs.writeFileSync(fileOutputPath, this.mdToHtml(data));
+        } else if (file.match(/.*\.(jpg|png|gif|ico|ttf|css|js)$/i)) {
+          fs.writeFileSync(fileOutputPath, fs.readFileSync(fileInputPath));
+        } else {
+          console.log('illegal file extension: ', file);
         }
+      });
 
-        this.converter = this.initConverter(true);
-        //listing all files
-        files.forEach((file) => {
-            let fileInputPath = path.resolve(inputDir, file);
-            let fileOutputPath = path.resolve(outputDir, file);
-            let stat = fs.statSync(fileInputPath);
-            if(stat.isDirectory()) {
-              this.build(fileInputPath, fileOutputPath);
-            } else if (file.match(/.*\.(md)|(MD)$/i)){
-              let data = fs.readFileSync(fileInputPath, "utf-8");
-              this.setFilePath(inputDir);
-              // Rename Readme.md to index.md
-              fileOutputPath.replace(/.*(README).md$/i, "$1index$2");
-              // Rename markdown file to html file
-              fileOutputPath = fileOutputPath.slice(0, -2) + 'html';
-              fs.writeFileSync(fileOutputPath, this.mdToHtml(data));
-            } else if (file.match(/.*\.(jpg|png|gif|ico|ttf|css|js)$/i)) {
-              fs.writeFileSync(fileOutputPath, fs.readFileSync(fileInputPath));
-            } else {
-              console.log("illegal file extension: ", file);
-            }
-        });
-
-        this.converter = this.initConverter(false);
+      this.converter = this.initConverter(false);
     });
 
     // Copy Resources
-    let resourcesInputDir = path.resolve(__dirname, "public"); 
-    let resourcesOutputDir = path.resolve(outputDir, "resources");
+    let resourcesInputDir = path.resolve(__dirname, '../public');
+    let resourcesOutputDir = path.resolve(outputDir, 'resources');
     this.build(resourcesInputDir, resourcesOutputDir);
 
     return true;
